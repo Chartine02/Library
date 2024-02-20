17c7f3f01cd13244c5df8f84f935fafd39f96731
Chartine02 <noellachartine125@gmail.com>
2024-02-20 11:40:38 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var library = [
+    {
+        title: "Digital Fortes",
+        author: "Dan Brown",
+        availability:true,
+
+    },
+
+    {
+        title: "LOng EAgle",
+        author: "unkown",
+        availability:false,
+
+    },
+
+    {
+        title: "Atomic Habits",
+        author: "James clear",
+        availability:false,
+
+    }
+]
