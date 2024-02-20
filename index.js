399a5c132ec872ffd047af5d80f30b29bd1f9d4c
Chartine02 <noellachartine125@gmail.com>
2024-02-20 11:46:26 +0200

    added addbook function

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ var library = [
         title: "Digital Fortes",
         author: "Dan Brown",
         availability:true,
-
     },
-
     {
         title: "LOng EAgle",
         author: "unkown",
         availability:false,
-
     },
-
     {
         title: "Atomic Habits",
         author: "James clear",
         availability:false,
-
     }
-]
+];
+
+const addBooks = (book) => {
+    console.log(library.length)
+    library.push(book)
+    console.log(library.length)
+};
